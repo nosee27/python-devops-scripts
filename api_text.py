513ada5253be18f3e4b1892f51734a660a18eed9
itlele <itlele@localhost.localdomain>
2026-04-07 16:06:56 +0800

    day23

---
+++ api_text.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import requests
+def main():
+    url1= "https://httpbin.org/ip"
+    try:
+        response=requests.get(url1,timeout=5)
+        response.raise_for_status()
+        data=response.json()
+        print(f"{data.get('origin','default')}")
+    except Exception as e:
+        print(e)
+    url2="https://api.github.com/repos/python/cpython"
+    try:
+        response=requests.get(url2,timeout=5)
+        response.raise_for_status()
+        data=response.json()
+        stars=data.get('stargazers_count','default')
+        print(f"stars are {stars}")
+    except Exception as e:
+        print(e)
+if __name__=="__main__":
+    main()
